19a7d37724b68d2b6b809108757723dd5d427bba
Obarski Maciej (TVN Digital) <mobarski@pl.grupa.iti>
2017-02-07 13:00:25 +0100

    focus in dialog, enter and esc in dialog

--- smash2/dialog.py
+++ smash2/dialog.py
@@ -3,12 +3,18 @@ window = Tk()
 window.wm_title("test")
 window.wm_iconbitmap("")
 
-def go():
-	print("go",v.get())
-	print(dir(v))
+def go(event=None):
+	print(e.get())
 	window.destroy()
 
-v = StringVar(value='TODO')
-e = Entry(window, textvariable=v,show="*").pack(pady=10, padx = 10, side=LEFT)
+v = StringVar(value='')
+e = Entry(window, textvariable=v,show="*")
+e.pack(pady=10, padx = 10, side=LEFT)
+e.focus_set()
+
 b = Button(window,text="go", width=5, command=go).pack(side=RIGHT)
+
+window.bind('<Return>', go)
+window.bind('<Escape>', lambda e:window.destroy())
+window.focus_force()
 window.mainloop()
