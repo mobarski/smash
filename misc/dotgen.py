48df610c8a798c40bb37a99171bd8af508ef45ca
mobarski <mobarski@gmail.com>
2017-02-19 02:55:14 +0100

    dot prototyping

--- misc/dotgen.py
+++ misc/dotgen.py
@@ -19,6 +19,22 @@ def scale(x,base=10,lo=1,hi=None,div=None):
 	val += lo
 	if hi and val>hi: return hi
 	return int(val)
+	
+def progress(x,n=5,black="&#9679;",white="&#9675;"):
+	if x<0: x=0
+	if type(x)==float: x=int(n*x)
+	if x>n: x=n
+	return black*x+white*(n-x)
+
+def rsplit(text,delimiters):
+	"recursive split"
+	d = delimiters[0]
+	s = text.split(d)
+	if len(delimiters)==1:
+		return s if len(s)>1 else s[0]
+	else:
+		x = [rsplit(x,delimiters[1:]) for x in s]
+		return x if len(x)>1 else x[0]
 #################################################
 
 # TODO - GNE GLOBALS
@@ -43,17 +59,17 @@ def load(filename, columns, skip_header=True, dlm='\t', rec='rec', lst_dlm='\s+'
 		out += [record(*r)]
 	return out
 
-nodes = load('data/conceptual-n.xls',"area graph cluster node label tags",rec='v')
+nodes = load('data/conceptual-n.xls',"area graph cluster node label tags val",rec='v')
 edges = load('data/conceptual-e.xls',"area graph cluster node node2 tags",rec='e',lst='node2')
 
 # TODO rename style to colors
-def generate(filename, cluster=True, direction='TD', style=''):
+def generate(filename, cluster=True, direction='TD', style='', value=''):
 	# generator
 	sys.stdout=open(filename,'w')
 	print('strict digraph {')
 	print('graph [rankdir={0} compound=true]'.format(direction))
 	print('edge [arrowhead=none]')
-	print('node [shape=box style="rounded,filled" fillcolor=white]')
+	print('node [shape=record style="rounded,filled" fillcolor=white]')
 	print('')
 
 	if style:
@@ -88,13 +104,17 @@ def generate(filename, cluster=True, direction='TD', style=''):
 
 ### XXX #########################
 import subprocess
-def render(dotname, outname, fmt='png'):
-	f = open(dotname,'r')
+def render(dot, out, fmt='png'):
+	f = open(dot,'r') if type(dot)==str else dot
 	cmd = "C:\\graphviz\\bin\\dot.exe"
-	rc=subprocess.call("{2} -T{1} -o{0}.{1}".format(outname,fmt,cmd),stdin=f,shell=True)
-	f.close()
+	rc=subprocess.call("{2} -T{1} -o{0}.{1}".format(out,fmt,cmd),stdin=f,shell=True)
+	f.close() if type(dot)==str else 0
 	return rc
 ###############################
 
 if __name__=="__main__":
-	generate('test.dot')
+	#generate('test.dot')
+	#render('test.dot','test')
+	#print(progress(0.99,3))
+	print(rsplit('a,1:b,2|z','|,:.'))
+	
