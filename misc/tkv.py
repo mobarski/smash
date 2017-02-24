e7b26770c231afb0d653d64586f9c8933ab8851f
mobarski <mobarski@gmail.com>
2017-02-24 20:28:26 +0100

    tkv serde choice, dict interface

--- misc/tkv.py
+++ misc/tkv.py
@@ -16,14 +16,34 @@ NoSQL interface to SQLite
 
 class SERDE:
 	"serialization-deserialization interface"
-	def __init__(self):
-		self.protocol = 3
-	def serialize(self,v):
+	def __init__(self,kind=''):
+		if kind=='':
+			ser = self.no_ser
+			de = self.no_de
+		elif kind=='pickle':
+			ser = self.pickle_ser
+			de = self.pickle_de
+			self.protocol = 3
+		elif kind=='json':
+			ser = self.json_ser
+			de = self.json_de
+		self.serialize = ser
+		self.deserialize = de
+	###
+	def json_ser(self,v):
+		return json.dumps(v)
+	def json_de(self,v):
+		return json.loads(v)
+	###
+	def pickle_ser(self,v):
 		return pickle.dumps(v,self.protocol)
-		#return v
-	def deserialize(self,v):
+	def pickle_de(self,v):
 		return pickle.loads(v)
-		#return v
+	###
+	def no_ser(self,v):
+		return v
+	def no_de(self,v):
+		return v
 
 
 class LINK:
@@ -53,7 +73,7 @@ class LINK:
 			self.cursor.execute('delete from tkv_link where link=? and k2=?',(link,k))
 
 
-# TODO separate file
+# TODO separate file + attach
 class HIST:
 	"history interface"
 	def __init__(self):
@@ -75,15 +95,16 @@ class STAR:
 	def add_fact(self,star,*keys):
 		pass
 
+
 # TODO HIST,SERDE mixin
 class TKV(SERDE,LINK,NOHIST):
 	"Table-Key-Value database"
-	def __init__(self,path=':memory:'):
+	def __init__(self,path=':memory:',serde='pickle'):
 		self.connection = sqlite3.connect(path)
 		self.cursor = self.connection.cursor()
 		self.cursor.execute('create table if not exists tkv (t,k,v,ts)')
 		self.cursor.execute('create unique index if not exists i_tkv on tkv (t,k)')
-		SERDE.__init__(self)
+		SERDE.__init__(self,serde)
 		LINK.__init__(self)
 		NOHIST.__init__(self)
 	### GET ###
@@ -91,12 +112,16 @@ class TKV(SERDE,LINK,NOHIST):
 		results = self.cursor.execute('select v from tkv where t=? and k=?',(t,k))
 		x = results.fetchone()
 		return self.deserialize(x[0]) if x else default
+	def __getitem__(self,x):
+		return self.get(*x)
 	### SET ###
 	def set(self,t,k,v):
 		ts = time()
 		val = self.serialize(v)
 		self.cursor.execute('insert or replace into tkv values (?,?,?,?)',(t,k,val,ts))
 		super().set(t,k,v,ts)
+	def __setitem__(self,x,v):
+		self.set(*x,v)
 	### ITER ###
 	def keys(self,t):
 		for k in self.cursor.execute('select distinct k from tkv where t=?',(t,)):
@@ -119,19 +144,22 @@ connect = TKV
 
 
 if __name__=="__main__":
-	db = connect()
-	db.set('usr',1,'bob')
-	db.set('usr',2,'alice')
-	db.set('usr',3,'charlie')
-	db.add_link('usr:knows:usr',1,2)
-	db.add_link('usr:knows:usr',1,3)
-	db.add_link('usr:knows:usr',2,3)
-	print(dict(db.items('usr')))
-	print(list(db.get_linked('usr:knows:usr',1)))
-	print(list(db.get_links('usr:knows:usr')))
-	db.delete_links(2,left=['usr:knows:usr'])
-	print(list(db.get_links('usr:knows:usr')))
-	print(list(db.values('usr')))
+	db = connect(serde='json')
+	if 1:
+		db.set('usr',1,'bob')
+		db.set('usr',2,'alice')
+		db.set('usr',3,'charlie')
+		db.add_link('usr:knows:usr',1,2)
+		db.add_link('usr:knows:usr',1,3)
+		db.add_link('usr:knows:usr',2,3)
+		#~ print(dict(db.items('usr')))
+		#~ print(list(db.get_linked('usr:knows:usr',1)))
+		#~ print(list(db.get_links('usr:knows:usr')))
+		#~ db.delete_links(2,left=['usr:knows:usr'])
+		#~ print(list(db.get_links('usr:knows:usr')))
+		#~ print(list(db.values('usr')))
+		db['usr',4] = 'david'
+		print(db['usr',4])
 	if 0:
 		CNT = 1000000
 		t0=time()
