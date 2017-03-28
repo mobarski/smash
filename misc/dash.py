534f991c1e232f911a0275ef930e6e0c6e2e11f9
mobarski <mobarski@gmail.com>
2017-03-28 20:33:10 +0200

    dash comments

--- misc/dash.py
+++ misc/dash.py
@@ -16,14 +16,14 @@ p_section = """ (?xms)
 	(.+?) (?= ^ \*{3} | \Z)		# body
 """
 
-def parse(text,cnt=0,default=None,comment='#<>!@+-;*',empty='.'):
+def parse(text,cnt=0,default=None,comment='#<>!@+-*|',empty='.',get_comments=False):
 	"parse body of dash section and return list of values for each row"
 	def field(x):
 		return x.strip() if x.strip()!=empty else default
 	def fields(line):
 		return [field(x) for x in re.split('\t+',line)]
 
-	lines = [x.strip() for x in re.split('\n\r|\r\n|\n|\r',text.strip()) if x.strip() and x.strip()[0] not in comment]		
+	lines = [x.strip() for x in re.split('\n\r|\r\n|\n|\r',text.strip()) if (x.strip() and x.strip()[0] in comment)==get_comments]
 
 	if cnt:
 		return [(fields(line)+[default]*cnt)[:cnt] for line in lines]
@@ -34,8 +34,13 @@ def sections(text):
 	"return [name,meta,body] for each section"
 	return re.findall(p_section,text)
 
-# SCITE:  file.patterns.diff=$(file.patterns.diff);*.dash
-# TODO: SCITE tab.width in .dash file
+__SCITE_CFG = """
+	-- DASH
+	file.patterns.diff=$(file.patterns.diff);*.dash
+	use.tabs.*.dash=1
+	tab.size.*.dash=8
+	indent.size.*.dash=8
+"""
 
 ###################################################
 
@@ -53,8 +58,9 @@ if __name__=="__main__":
 	# 	comment
 		bbb		zzz
 		ccc
+	!!	zzz	xxx	xxx
 		
 	"""
 	for name,meta,body in sections(dedent(test)):
+		#print(name,parse(body,comment='#',get_comments=False))
 		print(name,parse(body))
-
