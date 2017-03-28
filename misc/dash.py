567948fd146d5c0b230a07a491e75ed307288ec0
mobarski <mobarski@gmail.com>
2017-03-28 18:38:54 +0200

    dash

---
+++ misc/dash.py
@@ -0,0 +1,60 @@
+## DECLARATIVE AUTOMATION SHELL
+## (c) 2017 by mobarski (at) gmail (dot) com
+## licence: MIT
+
+import re
+
+# TODO rename parse
+# TODO parse_meta
+# TODO parse_comments
+# TODO more control over strip
+# TODO complex types
+
+p_section = """ (?xms)
+	^ \*{3} \s* (.+?) \s* \*{3}		# name
+	[ \t]* (.*?) [ \t]* $			# meta
+	(.+?) (?= ^ \*{3} | \Z)		# body
+"""
+
+def parse(text,cnt=0,default=None,comment='#<>!@+-;*',empty='.'):
+	"parse body of dash section and return list of values for each row"
+	def field(x):
+		return x.strip() if x.strip()!=empty else default
+	def fields(line):
+		return [field(x) for x in re.split('\t+',line)]
+
+	lines = [x.strip() for x in re.split('\n\r|\r\n|\n|\r',text.strip()) if x.strip() and x.strip()[0] not in comment]		
+
+	if cnt:
+		return [(fields(line)+[default]*cnt)[:cnt] for line in lines]
+	else:
+		return [fields(x) for x in lines]
+
+def sections(text):
+	"return [name,meta,body] for each section"
+	return re.findall(p_section,text)
+
+# SCITE:  file.patterns.diff=$(file.patterns.diff);*.dash
+# TODO: SCITE tab.width in .dash file
+
+###################################################
+
+if __name__=="__main__":
+	from textwrap import dedent
+	test = """
+	*** sekcja ***
+		to		jest		test
+		raz		2		trzy
+		# comment
+		wartosc	.		pusta
+
+	*** costam *** xxx
+		aaa
+	# 	comment
+		bbb		zzz
+		ccc
+		
+	"""
+	for name,meta,body in sections(dedent(test)):
+		print(name,parse(body))
+
